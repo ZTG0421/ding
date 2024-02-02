385ba5d845cd3d97a224a0d2b82153a9b8fa12f8
ZTG0421 <79940512+ZTG0421@users.noreply.github.com>
2024-02-02 14:13:26 +0800

    Update server.py
    增加延迟显示

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 import threading
 import time
-
+import re
 import keyboard
 import winsound
 
@@ -18,9 +18,9 @@ def listen(listen_address):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(listen_address)
     server.listen()
-    print('等待连接')
+    print('waiting for a client...')
     client, addr = server.accept()
-    print('connect:', addr)
+    print('OK  CONNECTED WITH {}', addr)
     return client
 
 
@@ -28,15 +28,24 @@ def listen(listen_address):
 def receive_ding(sock):
     while True:
         rec = sock.recv(1024)
-        print('recv:' + rec.decode('utf-8'))
-        if rec.decode('utf-8') == 'ding':
+        #当前毫秒时间戳 整数
+        current_time=int(round(time.time(),4)*10000)
+        rec=rec.decode('utf-8')
+        result = re.findall(r'[a-zA-Z]+', rec)
+        rec_time = re.findall(r'\d+', rec)
+        #对方毫秒时间戳 整数
+        rec_time=int(rec_time[0])
+        result = result[0]
+        if result == 'ding':
+            print('ding/'+str(current_time-rec_time)+'ms')
             threading.Thread(target=playDingSound).start()
 
 
 def send_ding(sock):
     while True:
         keyboard.wait('w')
-        sock.send('ding'.encode('utf-8'))
+        presend='ding'+str(int(round(time.time(),4)*10000))
+        sock.send(presend.encode('utf-8'))
         # 0.5s保护
         time.sleep(1)
 
